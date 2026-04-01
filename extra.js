a1e81c45f9a3d6bfda00a07a30921905ae154ccf
Nanyondo Sherinah Spellah <nsherinahspellah@gmail.com>
2026-04-01 07:59:46 +0300

    my recent changes

---
+++ extra.js
@@ -0,0 +1,9 @@
+let music = [
+    {singer: "adele", fanBase: 10000},
+    {singer: "ella", fanBase: 200},
+    {singer: "celine", fanBase: 500},
+    {singer: "sheebah", fanBase: 750}
+]
+let bigFanBase =music.filter((value, index) =>{
+console.log(value, )
+} ,0)
